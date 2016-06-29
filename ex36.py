393d79f5a69762e4c1d3da701e1634ac8463115a
Igor Quintela <jaigor@gmail.com>
2016-06-29 20:08:20 +0200

    Politics game engine

--- ex36.py
+++ ex36.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from random import randint   # used to get a random number
+from sys import exit
 
 class Actor:
 	"""Abstract Class for actors (players and enemies)"""
@@ -10,16 +11,20 @@ class Actor:
 	weakness = []
 
 	def set_life(self, life):
-		Actor.life = life
+		self.life = life
+		if life <= 0:
+			self.set_isDead(True)
 
 	def set_isDead(self, isDead):
-		Actor.isDead = isDead
+		self.isDead = isDead
+		if isDead == True:
+			game_over()
 
 	def set_attacks(self, attacks):
-		Actor.attacks = attacks
+		self.attacks = attacks
 
 	def set_weakness(self, weakness):
-		Actor.weakness = weakness
+		self.weakness = weakness
 
 	def get_life(self):
 		return self.life
@@ -65,24 +70,24 @@ class Enemy(Actor):
 		if idClass == "1" or idClass == "2":
 			rival = Derecha().get_rival
 			# random number to decide rival
-			num = randint(3,4)
-			if num == 3:
+			randomNum = randint(3,4)
+			if randomNum == 3:
 				self.set_attacks(Radical().get_attacks())
 				self.set_weakness(Radical().get_weakness())
 				self.name = Radical().get_name()
-			elif num == 4:
+			elif randomNum == 4:
 				self.set_attacks(Socialista().get_attacks())
 				self.set_weakness(Socialista().get_weakness())
 				self.name = Socialista().get_name()
 		elif idClass == "3" or idClass == "4":
 			rival = Izquierda().get_rival
 			# random number to decide rival
-			num = randint(1,2)
-			if num == 1:
+			randomNum = randint(1,2)
+			if randomNum == 1:
 				self.set_attacks(Ultra().get_attacks())
 				self.set_weakness(Ultra().get_weakness())
 				self.name = Ultra().get_name()
-			elif num == 2:
+			elif randomNum == 2:
 				self.set_attacks(Conservador().get_attacks())
 				self.set_weakness(Conservador().get_weakness())
 				self.name = Conservador().get_name()
@@ -121,7 +126,7 @@ class Ultra(Derecha):
 
 class Conservador(Derecha):
 	name = "Rajaos"
-	attacks = ["Demagogia", "Tribunal Justicia", "Manipulación Mediatica", "Ilegalizacion"]
+	attacks = ["Demagogia", "Tribunal Justicia", "Manipulacion Mediatica", "Ilegalizacion"]
 	weakness = ["Coherencia", "Socialismo", "Coctel", "Humor negro"]
 
 	def get_attacks(self):
@@ -165,7 +170,7 @@ class Socialista(Izquierda):
 
 def combat_menu():
 	print "------Debate Combat-------"
-	print "Escoge bando político:"
+	print "Escoge bando politico:"
 	print "Derecha"
 	print "1. Ultra"
 	print "2. Conservador"
@@ -184,26 +189,60 @@ def initial_stage(choice):
 	print "Se trata de un combate verbal."
 	print "Recuerda que la repeticion en politica es contraproducente"
 
+	nextTurn = "player"
 	print "Pulsa Intro para continuar!"
 	raw_input('> ')
-	combat_engine(player,enemy)
+	combat_interface(player,enemy, nextTurn)
 
-def combat_engine(player,enemy):
+def combat_interface(player,enemy, nextTurn):
 	print "\n"
 	print "Player Stats"
 	print "HP: %r" % player.get_life()
 	print "Ataques: "
 	i = 1
-	for ataque in player.get_attacks():
-		print "%d. %r" % (i, ataque)
-		i = i + 1
+	for attack in player.get_attacks():
+		print "%d. %r" % (i, attack)
+		i += 1
 
 	print "\n"
 	print "Enemy Stats"
 	print "HP: %r" % enemy.get_life()
 	print "\n"
-	print "Introduzca ataque a realizar: "
 
+	if nextTurn == "player":
+		attack_menu(player, enemy)
+	elif nextTurn == "enemy":
+		attack_enemy(player, enemy)
+
+def attack_menu(player, enemy):
+	print "Introduzca ataque a realizar: "
 	nextAttack = raw_input('> ')
+	combat_engine(nextAttack, player, enemy)
+
+def attack_enemy(player, enemy):
+	randomNum = randint(1,4)
+	combat_engine(randomNum, enemy, player)
+
+def combat_engine(nextAttack, player, enemy):
+	attack = player.get_attacks()[int(nextAttack) - 1]
+	print "%r uso %r." % (player.get_name(), attack)
+
+	if (any(weak == attack for weak in enemy.get_weakness())):
+		print "El ataque ha sido muy efectivo!!!"
+		enemy.set_life(enemy.get_life() - 50) 
+	else:
+		print "No ha causado mucho efecto"
+		enemy.set_life(enemy.get_life() - 30)
+		
+	if isinstance(player, Player):
+		nextTurn = "enemy"
+		combat_interface(player,enemy, nextTurn)
+	elif isinstance(player, Enemy):
+		nextTurn = "player"
+		combat_interface(enemy, player, nextTurn)
+
+def game_over():
+	print "******FIN DE LA PARTIDA******"
+	exit(0)
 
 combat_menu()
